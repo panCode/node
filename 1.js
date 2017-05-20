bb40e3e7a265448fe59fd1099017cb8cbc52be30
panCode <ricpandey92@gmail.com>
2017-05-20 22:45:53 +0530

    Delete 1.js

--- 1.js
+++
@@ -1,6 +0,0 @@
-var server= require('./server');
-var router = require('./router')
-// function route(pathname){
-// 	console.log("about to route request for "+pathname);
-// }
-server.start(router.route);
