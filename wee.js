184999094b71a4aed4e17555cfcf75081a9458d3
panCode <ricpandey92@gmail.com>
2017-05-20 23:03:47 +0530

    Delete wee.js

--- wee.js
+++
@@ -1,7 +0,0 @@
-var converter = require("csvtojson").converter;
-var fs = require("fs");
-var file = 	"./airline";
-csvConverter.on("end_parsed",function(jsonObj){
-    console.log(jsonObj); 
-});
-fs.createReadStream(csvFileName).pipe(csvConverter);
